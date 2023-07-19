eff4a0e8310367d3f9bf7f3e554a266ef691db6e
clenn <acrivopoulosdotcom@gmail.com>
2023-07-19 17:37:09 +0200

    Initial commit

---
+++ src/main/java/com/example/thirdweekstreams/ThirdWeekStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdweekstreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdWeekStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdWeekStreamsApplication.class, args);
+	}
+
+}
